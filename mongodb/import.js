717377ef158f6ae367a55f41f3b5eb280454d619
Valentin Cocaud <v.cocaud@gmail.com>
2017-01-18 16:41:23 +0100

    Simple importation

--- mongodb/import.js
+++ mongodb/import.js
@@ -1,30 +1,39 @@
-var mongodb = require('mongodb');
-var csv = require('csv-parser');
-var fs = require('fs');
+const mongodb = require('mongodb')
+const csv = require('csv-parser')
+const fs = require('fs')
 
-var MongoClient = mongodb.MongoClient;
-var mongoUrl = 'mongodb://localhost:27017/911-calls';
+const MongoClient = mongodb.MongoClient
+const mongoUrl = 'mongodb://localhost:27017/911-calls'
 
-var insertCalls = function(db, callback) {
-    var collection = db.collection('calls');
+const formatData = data => {
+	const {lat, lng, title, timeStamp: date, zip, twp : town, addr} = data
+	const splitPoint = title.indexOf(':')
+	const category = title.substring(0, splitPoint).toUpperCase()
+	const description = title.substring(splitPoint + 1, title.length).trim()
+	return {
+		category, description, date,
+		location : [parseFloat(lat), parseFloat(lng)], addr, town, zip
+	}
+}
 
-    var calls = [];
-    fs.createReadStream('../911.csv')
-        .pipe(csv())
-        .on('data', data => {
-            var call : {}; // TODO créer l'objet call à partir de la ligne
-            calls.push(call);
-        })
-        .on('end', () => {
-          collection.insertMany(calls, (err, result) => {
-            callback(result)
-          });
-        });
+const insertCalls = function(db, callback) {
+	const collection = db.collection('calls')
+
+	const calls = []
+	fs.createReadStream('../simple-911.csv')
+		.pipe(csv())
+		.on('data', data => calls.push(formatData(data)))
+		.on('end', () => {
+			collection.insertMany(calls, (err, result) => {
+				if(err) console.error(err)
+				else callback(result)
+			})
+		})
 }
 
 MongoClient.connect(mongoUrl, (err, db) => {
-    insertCalls(db, result => {
-        console.log(`${result.insertedCount} calls inserted`);
-        db.close();
-    });
-});
+	insertCalls(db, result => {
+		console.log(`${result.insertedCount} calls inserted`)
+		db.close()
+	})
+})
